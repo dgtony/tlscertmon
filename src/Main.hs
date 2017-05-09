e694b94ca6642cde33d5f1453d6f686b60c410ae
Anton Dort-Golts <dortgolts@gmail.com>
2017-05-09 12:25:46 +0300

    Feat: certificate expiration time

--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,7 @@ import qualified Control.Monad.Trans.Either as ET
 import qualified Numeric as N
 import qualified Data.Hash.MD5 as MD5
 
+import qualified Data.Time.Clock as Clock
 
 --serverHost = "01.mqtt.syncleo-iot.com"
 --serverPort = 8883
@@ -24,7 +25,14 @@ type Port = Int
 type SrvKey = String
 
 
-data ProcErrors = ConnectFail | RetrieveFail | PKObtainFail deriving (Eq, Show)
+data ProcErrors = ConnectFail
+                    | RetrieveFail
+                    | PKObtainFail deriving (Eq, Show)
+
+
+data CrtExpiration = Days Int
+                    | CrtNotValidYet
+                    | CrtExpired deriving (Eq, Show)
 
 
 getServerCrt :: Host -> Port -> IO (Either ProcErrors X509.X509)
@@ -37,16 +45,16 @@ getServerCrt h p = do
     where
         getSrvCrtProc :: Host -> Port -> IO (Maybe X509.X509)
         getSrvCrtProc h p = do
-        -- connect
-        addrInfo <- S.getAddrInfo Nothing (Just h) (Just $ show p)
-        let serverAddr = head addrInfo
-        sock <- S.socket (S.addrFamily serverAddr) S.Stream S.defaultProtocol
-        S.connect sock (S.addrAddress serverAddr)
-        -- wrap socket
-        ctx <- SSL.context
-        wrappedSSLSocket <- SSL.connection ctx sock
-        _ <- SSL.connect wrappedSSLSocket    
-        SSL.getPeerCertificate wrappedSSLSocket
+            -- connect
+            addrInfo <- S.getAddrInfo Nothing (Just h) (Just $ show p)
+            let serverAddr = head addrInfo
+            sock <- S.socket (S.addrFamily serverAddr) S.Stream S.defaultProtocol
+            S.connect sock (S.addrAddress serverAddr)
+            -- wrap socket
+            ctx <- SSL.context
+            wrappedSSLSocket <- SSL.connection ctx sock
+            _ <- SSL.connect wrappedSSLSocket
+            SSL.getPeerCertificate wrappedSSLSocket
 
 
 getPubKey :: X509.X509 -> IO (Either ProcErrors RSA.RSAPubKey)
@@ -73,10 +81,32 @@ keySignature = getMD5Sum . prepend . map toUpper . int2hex . RSA.rsaN
 getServerKey :: Host -> Port -> IO (Either ProcErrors RSA.RSAPubKey)
 getServerKey h p = ET.runEitherT $ do
     crtRes <- lift $ getServerCrt h p
-    crt <- ET.hoistEither crtRes
-    pkRes <- lift $ getPubKey crt
-    pk <- ET.hoistEither pkRes
+    crt    <- ET.hoistEither crtRes
+    pkRes  <- lift $ getPubKey crt
+    pk     <- ET.hoistEither pkRes
     return pk
+
+
+getCrtTime :: Host -> Port -> IO (Either ProcErrors CrtExpiration)
+getCrtTime h p = ET.runEitherT $ do
+    crtRes    <- lift $ getServerCrt h p
+    crt       <- ET.hoistEither crtRes
+    notBefore <- lift $ X509.getNotBefore crt
+    notAfter  <- lift $ X509.getNotAfter crt
+    daysLeft  <- lift $ getDaysLeft notBefore notAfter
+    return daysLeft
+
+
+getDaysLeft :: Clock.UTCTime -> Clock.UTCTime -> IO CrtExpiration
+getDaysLeft notBefore notAfter = do
+        currentTime <- Clock.getCurrentTime
+        return $ daysLeft currentTime notBefore notAfter
+    where
+        secToDays = floor . fromRational . (/(3600 * 24)) . toRational
+        daysLeft currentTime notBefore notAfter
+                | currentTime < notBefore = CrtNotValidYet
+                | currentTime > notAfter = CrtExpired
+                | otherwise = Days $ secToDays $ Clock.diffUTCTime notAfter currentTime
 
 
 checkServerKey :: Host -> Port -> SrvKey -> IO ()
@@ -84,17 +114,31 @@ checkServerKey h p refKey = do
     keyRes <- getServerKey h p
     case keyRes of
         -- TODO set values in prometheus instead of printing
-        Left err -> putStrLn $ "key check error: " ++ errToMetric err
+        Left err  -> putStrLn $ "key check error: " ++ errToMetric err
         Right key -> if refKey == keySignature key
                         then putStrLn "ok, key matches!"
                         else putStrLn "failure, key doesn't match"
 
 
+checkServerCrt :: Host -> Port -> IO ()
+checkServerCrt h p = do
+    daysLeft <- getCrtTime h p
+    case daysLeft of
+        Left err  -> putStrLn $ "key check error: " ++ errToMetric err
+        Right res -> putStrLn $ "certificate expiration check: " ++ daysToMetric res
+
+
 -- TODO use prometheus gauge values instead of strings: i.e Int
 errToMetric :: ProcErrors -> String
-errToMetric ConnectFail  = "server connection error"
-errToMetric RetrieveFail = "certificate retrieval error"
-errToMetric PKObtainFail = "public key retrieval error"
+errToMetric ConnectFail     = "server connection error"
+errToMetric RetrieveFail    = "certificate retrieval error"
+errToMetric PKObtainFail    = "public key retrieval error"
+
+daysToMetric :: CrtExpiration -> String
+daysToMetric CrtNotValidYet = "certificate not valid yet"
+daysToMetric CrtExpired     = "certificate expired"
+daysToMetric (Days n)       = "days to expiration: " ++ show n
+
 
 
 serverHost = "127.0.0.1"
@@ -102,14 +146,7 @@ serverPort = 11011
 
 
 main = withOpenSSL $ do
-    -- TODO add cert expiration time
-    {-
-    pkRes <- getServerKey serverHost serverPort
-    case pkRes of
-        Left exc -> putStrLn $ "certificate verification failed: " ++ show exc
-        Right pk -> putStrLn $ "key signature: " ++ keySignature pk
-    -}
     let sampleKey = "790827182fb3a1fd395c69976778d34e"
     checkServerKey serverHost serverPort sampleKey
-
+    checkServerCrt serverHost serverPort
 
